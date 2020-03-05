2bd5da0635e0fc17e5ba32a7a3081a15fe5d5872
amit328 <51810663+amit328@users.noreply.github.com>
2020-03-05 14:16:53 +0530

    countdown

--- js/count.js
+++ js/count.js
@@ -75,7 +75,7 @@
 //Provide the plugin settings
 $("#countdown").countdown({
         //The countdown end date
-        date: " 5 March 2020 12:00:00",
+        date: " 6 March 2020 16:00:00",
 
         // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
         format: "on"
